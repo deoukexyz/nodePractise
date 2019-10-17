f76af4ca9416b64f451444ea14711a834a720f15
deoukexyz <vewi@net2mail.top>
2019-10-17 19:19:01 +0530

    first commit

---
+++ api/routes/products.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
+
+
+router.get('/', (req, res, next) => {
+    Product.find()
+        .select('name price _id')
+        .exec()
+        .then(docs => {
+            // console.log(docs);
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/products/' + doc._id
+                        }
+                    }
+                })
+            }
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(docs)
+            res.status(500).json({
+                error: err
+            })
+        })
+});
+
+router.post('/', (req, res, next) => {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+
+    });
+    product.save().then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: "Handling POST requrests to /products",
+            createdProduct: result
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });
+
+
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log(doc);
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(500).json({ message: 'No valid entry found for provided ID' })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+})
+
+router.patch('/:productId', (req, res, next) => {
+
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({ _id: id }, { $set: updateOps })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
+});
+
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.remove({
+        _id: id
+    })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
+});
+
+module.exports = router;
